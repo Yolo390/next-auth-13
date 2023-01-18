8e2e285df74ca1566fe0615629721cb5e52acd6d
Flo Slv <florent3969@hotmail.fr>
2023-01-18 22:39:40 +0100

    🟦 feat: register API

---
+++ src/pages/api/signUp/index.js
@@ -0,0 +1,25 @@
+// import { signUp } from "../../../../prisma/utils/users.js";
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    try {
+      const data = req.body;
+
+      const { user, error } = await signUp(data);
+
+      if (error) throw new Error(error);
+
+      return res.status(200).json({
+        message: "User successfully created !",
+        user,
+      });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
+
+  res.setHeader("Allow", ["POST"]);
+  res.status(405).end(`Method ${req.method} is not allowed.`);
+};
+
+export default handler;
