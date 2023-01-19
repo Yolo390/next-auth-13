cc49087da830ee1875db1d954a178968295d3abc
Flo Slv <florent3969@hotmail.fr>
2023-01-19 02:39:58 +0100

    🟦 feat: add Signin Prisma function

--- src/lib/prisma/utils/users.js
+++ src/lib/prisma/utils/users.js
@@ -2,6 +2,45 @@ import bcrypt from "bcrypt";
 
 import prisma from "@/lib/prisma/utils/prismaClient.js";
 
+export const signIn = async (user) => {
+  try {
+    const email = user.email.trim();
+    const password = user.password.trim();
+
+    // Check if email and password are not empty.
+    if (email === "" || password === "")
+      return { error: "Fieds can not be empty !" };
+
+    // Check if email is valid.
+    const regEx =
+      /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+
+    if (!email.match(regEx)) return { error: "Email is not valid !" };
+
+    const existingUser = await prisma.user.findUniqueOrThrow({
+      where: { email: email },
+    });
+
+    if (existingUser) {
+      // Compare hash of passwords.
+      if (!bcrypt.compareSync(password, existingUser.password))
+        return { error: "Bad password !" };
+
+      return {
+        user: {
+          id: existingUser.id,
+          name: existingUser.name,
+          email: existingUser.email,
+          emailVerified: existingUser.emailVerified,
+          image: existingUser.image,
+        },
+      };
+    }
+  } catch (error) {
+    return { error };
+  }
+};
+
 export const signUp = async (user) => {
   try {
     const name = user.name.trim();
@@ -19,7 +58,7 @@ export const signUp = async (user) => {
     const regEx =
       /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
 
-    if (!email.match(regEx)) return { error: "Email is not valid" };
+    if (!email.match(regEx)) return { error: "Email is not valid !" };
 
     // Check if user already exist.
     const existingUser = await prisma.user.findUnique({
