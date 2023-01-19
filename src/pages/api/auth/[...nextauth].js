47386ea9a345248161a7768b5141f1ed323fc12d
Flo Slv <florent3969@hotmail.fr>
2023-01-19 02:40:40 +0100

    🟦 feat: [...nextauth].js api

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,9 +1,41 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-import prisma from "@/lib/prismadb";
+import prisma from "@/lib/prisma/utils/prismaClient.js";
+import { signIn } from "@/lib/prisma/utils/users.js";
 
-export default NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [],
-});
+const authHandler = async (req, res) => {
+  return await NextAuth(req, res, {
+    providers: [
+      CredentialsProvider({
+        async authorize(credentials, req) {
+          if (req.method === "POST") {
+            try {
+              const { user, error } = await signIn(credentials);
+
+              if (error) throw new Error(error);
+
+              return user;
+            } catch (error) {
+              return res.status(500).json({ error: error.message });
+            }
+          }
+
+          res.setHeader("Allow", ["POST"]);
+          res.status(405).end(`Method ${req.method} is not allowed.`);
+        },
+      }),
+    ],
+    adapter: PrismaAdapter(prisma),
+    pages: {
+      signIn: "/signin",
+    },
+    session: {
+      strategy: "jwt", // JSON Web Token
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+};
+
+export default authHandler;
