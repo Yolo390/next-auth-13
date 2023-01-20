72cdf6aac4cf216db7609065380a52a1ef34dfdb
Flo Slv <florent3969@hotmail.fr>
2023-01-20 22:09:58 +0100

    🟨 style: overflow hidden to body

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,7 +6,7 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <head />
-      <body className="h-screen bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 via-gray-100 to-gray-900">
+      <body className="h-screen overflow-hidden bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 via-gray-100 to-gray-900">
         <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
